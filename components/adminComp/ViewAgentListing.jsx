fea9456cb1d3cc3d9782025d139f2eaf9f5b1393
Dhruthi Spoorthi <dhruthispoorthi25@gmail.com>
2026-03-01 11:11:07 +0530

    add;committed changes.

--- components/adminComp/ViewAgentListing.jsx
+++ components/adminComp/ViewAgentListing.jsx
@@ -1,13 +1,13 @@
 "use client";
 
 export default function ViewAgentListing({ listing }) {
-  console.log(listing);
+  
   return (
     <>
       {/* Outer container */}
       <div>
         {/* HEAD */}
-        <div className="grid grid-cols-[5fr_1fr_1fr_1fr] px-2 py-2 rounded-t-md bg-gray-50 font-medium text-lg">
+        <div className="grid grid-cols-[5fr_1fr_1fr_1fr] pl-5 py-2 rounded-t-md bg-blue-100 font-medium text-lg">
           <span className="">Property title</span>
           <span className="text-center">Location</span>
           <span className="text-center">Status</span>
@@ -16,8 +16,8 @@ export default function ViewAgentListing({ listing }) {
         {/* ROWS */}
         {listing.map((l, index) => (
           <button key={index} className="w-full text-left">
-            <div className="grid grid-cols-[5fr_1fr_1fr_1fr]">
-              <span>{l.title}</span>
+            <div className="grid grid-cols-[5fr_1fr_1fr_1fr] pl-5 py-2 items-center shadow-lg rounded-lg mt-1">
+              <span className="text-xl font-medium"s>{l.title}</span>
               <span className="text-center">{l.location.area}</span>
               <span
                 className={`px-6 py-1 rounded-full text-sm w-fit place-self-center text-center ${
