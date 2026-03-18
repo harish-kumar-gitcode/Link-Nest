5ecbdc9092988529ef662ad349728529c78b3842
Harish Kumar <harish15kumar06@gmail.com>
2026-03-18 09:04:48 +0530

    feat: added GA for homepage.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata = {
   metadataBase: new URL("https://link-nest-mu.vercel.app"),
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <GoogleAnalytics gaId="G-V3MH6PDSX2" />
     </html>
   );
 }
